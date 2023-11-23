d120faf2fa651a15257d75ffa19a6b66e6e966f1
lux00leo <lux00leo@gmail.com>
2023-11-23 21:27:21 +0900

    list card

---
+++ src/components/molecules/study/list/StudyCard.vue
@@ -0,0 +1,37 @@
+<script setup>
+import {onMounted, ref} from "vue";
+import {URLCONST} from "@/constants/APIconst";
+import Img from "@/components/molecules/common/Img.vue";
+  const localStudyInfo = ref({
+  })
+  const props = defineProps({
+    studyInfo: Object
+  })
+  onMounted(()=>{
+    console.log("hello child")
+    localStudyInfo.value = props.studyInfo;
+  })
+</script>
+
+<template>
+  <div class="study-card">
+    <div class="study-card-thumb">
+      <Img :content="studyInfo.thumb" />
+    </div>
+    <div class="study-card-content">
+      <div>{{localStudyInfo.title}}</div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+  .study-card{
+    max-width: 400px;
+  }
+  .study-card-thumb img{
+    width: 100%;
+    aspect-ratio: 3/2;
+    object-fit: fill;
+    display: block;
+  }
+</style>
--- src/utils/fetch/studyFetch.js
+++ src/utils/fetch/studyFetch.js
@@ -1,6 +1,14 @@
-import {axiClient} from "@/utils/globalAxios";
+import {axiClient, axiValid} from "@/utils/globalAxios";
 import {URLCONST} from "@/constants/APIconst";
 export const getLocations = ()=>{
     return axiClient.get(URLCONST.STUDY_LOCATIONS)
         .then(res=> res.data)
 }
+
+export const getStudies = async (sort) => {
+    return await axiValid.get(URLCONST.STUDY_LIST+`?sort=${sort}`)
+        .then(res => {
+            console.log(res.data)
+            return res.data;
+        })
+};
