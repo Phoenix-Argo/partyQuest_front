9aeefa7920e77a15df9d31ce0716beabe877bf6f
Jincchus <chopoo2001@gmail.com>
2023-11-23 12:43:33 +0900

    20231123 view

--- src/components/molecules/study/StudyView.vue
+++ src/components/molecules/study/StudyView.vue
@@ -1,3 +1,264 @@
+<template>
+  <!-- Open Content -->
+  <section class="bg-light">
+    <div class="container pb-5">
+      <div class="row">
+        <div class="col-lg-5 mt-5">
+          <div class="card mb-3" v-if="studyView.thumb != null">
+            <Img
+              :content="studyView.thumb"
+              class="card-img img-fluid"
+              id="studyThumb"
+            />
+          </div>
+        </div>
+
+        <!-- col end -->
+        <div class="col-lg-7 mt-5">
+          <div class="card" id="studyBase">
+            <div class="card-body">
+              <h1 class="h1">{{ studyView.title }}</h1>
+
+              <div class="row pb-3" id="studyHostInfo">
+                <div class="col-lg-5 mt-5" v-if="studyView.avatar != null">
+                  <Img :content="studyView.avatar" id="hostAvatar" />
+                </div>
+                <div class="col-lg-7 mt-5">
+                  <div id="viewHostNickBio">
+                    <h3>{{ studyView.hostNickName }}</h3>
+                    <h5>{{ studyView.hostBio }}</h5>
+                  </div>
+                </div>
+              </div>
+
+              <div class="row pb-3" id="skillSection">
+                <label> 요구 기술 스택</label>
+                <div
+                  class="col-lg-2 d-grid"
+                  v-for="skill in studyView.smallCate"
+                >
+                  <button
+                    class="btn btn-block btn-outline-danger"
+                    style="
+                      white-space: nowrap;
+                      text-overflow: ellipsis;
+                      margin-top: 10px;
+                      margin-right: 5px;
+                    "
+                  >
+                    {{ "#" + skill.smallCateName }}
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- Close Content -->
+
+  <!-- Start Detail Content-->
+  <section class="py-5">
+    <div class="container pb-5">
+      <div class="row">
+        <div class="col-lg-8 mt-5">
+          <div
+            :innerHTML="studyView.description"
+            style="font-size: large"
+          ></div>
+          <img src="http://via.placeholder.com/800x2000" alt="" />
+        </div>
+
+        <div class="col-lg-3 mt-5 sid-nav">
+          <div class="description">
+            <h6>상세정보</h6>
+            <ul class="list-unstyled pb-3" id="studyInfo">
+              <li>
+                참여인원 :
+                <ol>
+                  <!--현재인원 / 총원-->
+                  {{
+                    studyView.curMembersSize
+                  }}
+                  /
+                  {{
+                    studyView.memberUpperLimit
+                  }}
+                </ol>
+              </li>
+              <li>승인제 or 선착순</li>
+              <ol v-if="studyView.recruitOption === 'PNP'">
+                승인제
+              </ol>
+              <ol v-else>
+                선착순
+              </ol>
+              <li>모임 기간</li>
+              <ol>
+                {{
+                  studyView.studyStartDate
+                }}~
+                {{
+                  studyView.studyEndDate
+                }}
+              </ol>
+              <li>모임 장소</li>
+              <ol>
+                {{
+                  studyView.partyLocation
+                }}
+              </ol>
+            </ul>
+            <div class="icons">
+              <a class="icon-mail"><IconEnvelope></IconEnvelope></a>&nbsp;
+              <a class="icon-heart" @click="updateLike"
+                ><IconHeart v-if="!isFilled"></IconHeart
+                ><IconHeartFill v-else></IconHeartFill></a
+              >&nbsp; <a class="icon-angry"><IconAngry></IconAngry></a>&nbsp;
+            </div>
+            <button class="btn-accept">참가하기</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <!-- End Detail Content-->
+
+  <!-- Start Member Info -->
+  <section class="py-5">
+    <div class="container pb-5">
+      <h4>파티원 소개</h4>
+      <p>{{ studyView.studyMemberInfo }} 보류</p>
+      <div
+        class="row"
+        v-for="studyMember in studyView.studyMemberInfo"
+        :key="studyMember.memberId"
+      >
+        <div class="col-lg-2"></div>
+        <div class="col-lg-2" id="partyMemberMain">
+          <Img :content="studyView.avatar" id="hostAvatar" />
+          <img src="http://via.placeholder.com/150.jpg" id="memberAvatar" />
+        </div>
+        <div class="col-lg-5" style="border: 1px solid black">
+          <h3>memberNick</h3>
+          <h4>memberBioInfo</h4>
+          <p>관심 기술 스택</p>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <!-- End Member Info -->
+  <!-- Start Article -->
+
+  <section class="container py-9">
+    <div class="row text-left pt-3">
+      <div class="col-lg-12 m-auto">
+        <p id="title1">비슷한 모임</p>
+      </div>
+    </div>
+    <div class="container">
+      <div class="row row-cols-1 row-cols-md-2 row-cols-lg-4">
+        <div class="col">
+          <div class="card px-4 py-2">
+            <div class="div1 row py-2 px-2">
+              <img
+                src="http://via.placeholder.com/400.jpg"
+                height="130px"
+                width="auto"
+                alt=""
+              />
+            </div>
+            <div class="py-2">
+              <p id="desc">모임 title</p>
+
+              <div class="d-flex">
+                <button
+                  class="btnArticle d-flex ml-auto px-3 font-weight-bold darkWhite"
+                >
+                  바로가기
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+        <!-- 위의 카드를 4번 복사하여 총 4개의 카드가 있는 행을 생성하세요 -->
+        <div class="col">
+          <div class="card px-4 py-2">
+            <div class="div1 row py-2 px-2">
+              <img
+                src="http://via.placeholder.com/400.jpg"
+                height="130px"
+                width="auto"
+                alt=""
+              />
+            </div>
+            <div class="py-2">
+              <p id="desc">모임 title</p>
+
+              <div class="d-flex">
+                <button
+                  class="btnArticle d-flex ml-auto px-3 font-weight-bold darkWhite"
+                >
+                  바로가기
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col">
+          <div class="card px-4 py-2">
+            <div class="div1 row py-2 px-2">
+              <img
+                src="http://via.placeholder.com/400.jpg"
+                height="130px"
+                width="auto"
+                alt=""
+              />
+            </div>
+            <div class="py-2">
+              <p id="desc">모임 title</p>
+              <div class="d-flex">
+                <button
+                  class="btnArticle d-flex ml-auto px-3 font-weight-bold darkWhite"
+                >
+                  바로가기
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="col">
+          <div class="card px-4 py-2">
+            <div class="div1 row py-2 px-2">
+              <img
+                src="http://via.placeholder.com/400.jpg"
+                height="130px"
+                width="auto"
+                alt=""
+              />
+            </div>
+            <div class="py-2">
+              <p id="desc">모임 title</p>
+
+              <div class="d-flex">
+                <button
+                  class="btnArticle d-flex ml-auto px-3 font-weight-bold darkWhite"
+                >
+                  바로가기
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  <!-- End Article -->
+</template>
+
 <script setup>
 import axios from "axios";
 import { onBeforeMount } from "vue";
@@ -11,11 +272,11 @@ import IconAngry from "../../icons/IconAngry.vue";
 import IconHeartFill from "../../icons/IconHeartFill.vue";
 import { useAuthStore } from "../../../stores/authStore";
 import Img from "../common/Img.vue";
-import {axiValid, getValidatedAxios} from "@/utils/globalAxios";
+import { axiValid, getValidatedAxios } from "@/utils/globalAxios";
 
 const BASE_URL = "/api/study";
 
-const { user,accessToken } = useAuthStore();
+const { user, accessToken } = useAuthStore();
 
 // 라우터 인스턴스 가져오기
 const route = useRoute();
@@ -63,221 +324,6 @@ const updateLike = async () => {
     console.error(error);
   }
 };
-
 </script>
-<template>
-  <body>
-    <div class="container py-5">
-      <div class="row py-5">
-        <div class="viewTitle">
-          <div
-            class="titleThumb"
-            id="titleThumb"
-            v-if="studyView.thumb !== null"
-          >
-            <Img :content="studyView.thumb" />
-          </div>
-          <div class="col-md-9 m-auto" id="hostProfile">
-            <div id="hostPicture">
-              <Img :content="studyView.avatar" id="myPicture" />
 
-              <div id="hostNick" class="form-text">
-                {{ studyView.hostNickName }}
-              </div>
-            </div>
-            <div id="viewInfo">
-              <label class="form-label" id="viewTitle">{{
-                studyView.title
-              }}</label>
-              <a href="#">
-                <path
-                  d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708l-3-3zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207l6.5-6.5zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.499.499 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11l.178-.178z"
-                />
-              </a>
-            </div>
-          </div>
-          <div id="wantSkill" class="form-text">요구 기술 스택</div>
-        </div>
-        <!-- information -->
-        <div class="infomation">
-          <div class="contents">
-            <p>{{ studyView.description }}</p>
-            <img src="http://placehold.it/800X1000" alt="#" />
-          </div>
-          <div class="side-nav">
-            <div class="description">
-              <h6>상세정보</h6>
-              <ul class="list-unstyled pb-3" id="studyInfo">
-                <li>
-                  참여인원 :
-                  <ol>
-                    <!--현재인원 / 총원-->
-                    {{
-                      studyView.curMembersSize
-                    }}
-                    /
-                    {{
-                      studyView.memberUpperLimit
-                    }}
-                  </ol>
-                </li>
-                <li>승인제 or 선착순</li>
-                <ol v-if="studyView.recruitOption === 'PNP'">
-                  승인제
-                </ol>
-                <ol v-else>
-                  선착순
-                </ol>
-                <li>모임 기간</li>
-                <ol>
-                  {{
-                    studyView.studyStartDate
-                  }}~
-                  {{
-                    studyView.studyEndDate
-                  }}
-                </ol>
-                <li>모임 장소</li>
-                <ol>
-                  {{
-                    studyView.partyLocation
-                  }}
-                </ol>
-              </ul>
-              <div class="icons">
-                <a class="icon-mail"><IconEnvelope></IconEnvelope></a>&nbsp;
-                <a class="icon-heart" @click="updateLike"
-                  ><IconHeart v-if="!isFilled"></IconHeart
-                  ><IconHeartFill v-else></IconHeartFill></a
-                >&nbsp; <a class="icon-angry"><IconAngry></IconAngry></a>&nbsp;
-              </div>
-              <button class="btn-accept">참가하기</button>
-            </div>
-          </div>
-        </div>
-        <!-- Start viewMemberProfile -->
-        <hr class="sectionLine" />
-        <section>
-          <div class="row py-5">
-            <label class="partyMemberIntroduce"> 파티원 소개 </label>
-
-            <div
-              class="col-md-9 m-auto"
-              id="partyMemberMain"
-              v-for="studyMemberInfo in studyView.studyMemberInfo"
-              :key="studyMemberInfo.memberId"
-            >
-              <p>{{ studyMemberInfo.memberId }}</p>
-              <div id="partyMemberPicture">
-                <img src="{}" alt="#" />
-              </div>
-              <div id="partyMemberProfileName">
-                <label class="form-label" id="nickName">{{
-                  studyMemberInfo.memberNickName
-                }}</label>
-                <a href="#">
-                  <svg
-                    xmlns="http://www.w3.org/2000/svg"
-                    width="16"
-                    height="16"
-                    fill="currentColor"
-                    class="bi bi-pencil-fill"
-                    viewBox="0 0 16 16"
-                  >
-                    <path
-                      d="M12.854.146a.5.5 0 0 0-.707 0L10.5 1.793 14.207 5.5l1.647-1.646a.5.5 0 0 0 0-.708l-3-3zm.646 6.061L9.793 2.5 3.293 9H3.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.5h.5a.5.5 0 0 1 .5.5v.207l6.5-6.5zm-7.468 7.468A.5.5 0 0 1 6 13.5V13h-.5a.5.5 0 0 1-.5-.5V12h-.5a.5.5 0 0 1-.5-.5V11h-.5a.5.5 0 0 1-.5-.5V10h-.5a.499.499 0 0 1-.175-.032l-.179.178a.5.5 0 0 0-.11.168l-2 5a.5.5 0 0 0 .65.65l5-2a.5.5 0 0 0 .168-.11l.178-.178z"
-                    />
-                  </svg>
-                </a>
-              </div>
-              <div id="partyMemberProfileDetail" class="form-text">
-                {{ studyMemberInfo.memberBio }}
-              </div>
-            </div>
-          </div>
-          <button type="button" id="moreViewBtn" class="btn btn-danger">
-            더보기
-          </button>
-        </section>
-        <!-- End viewMemberProfile Description -->
-        <hr class="sectionLine" />
-        <!-- 관련모임 -->
-        <div class="container">
-          <p>관련모임</p>
-          <div class="row row-cols-1 row-cols-md-2 row-cols-lg-4">
-            <div class="col">
-              <div class="card px-4 py-2">
-                <div class="div1 row py-2 px-2">
-                  <img src="http://placehold.it/400X300" alt="#" />
-                </div>
-                <div class="py-2">
-                  <p id="desc">모임</p>
-                  <div class="d-flex">
-                    <button
-                      class="btnArticle d-flex ml-auto px-3 font-weight-bold darkWhite"
-                    >
-                      바로가기
-                    </button>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <!-- 위의 카드를 4번 복사하여 총 4개의 카드가 있는 행을 생성하세요 -->
-            <div class="col">
-              <div class="card px-4 py-2">
-                <div class="div1 row py-2 px-2">
-                  <img src="http://placehold.it/400X300" alt="#" />
-                </div>
-                <div class="py-2">
-                  <p id="desc">모임</p>
-                  <div class="d-flex">
-                    <button
-                      class="btnArticle d-flex ml-auto px-3 font-weight-bold darkWhite"
-                    >
-                      바로가기
-                    </button>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div class="col">
-              <div class="card px-4 py-2">
-                <div class="div1 row py-2 px-2">
-                  <img src="http://placehold.it/400X300" alt="#" />
-                </div>
-                <div class="py-2">
-                  <p id="desc">모임</p>
-                  <div class="d-flex">
-                    <button
-                      class="btnArticle d-flex ml-auto px-3 font-weight-bold darkWhite"
-                    >
-                      바로가기
-                    </button>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div class="col">
-              <div class="card px-4 py-2">
-                <div class="div1 row py-2 px-2">
-                  <img src="http://placehold.it/400X300" alt="#" />
-                </div>
-                <div class="py-2">
-                  <p id="desc">모임</p>
-                  <div class="d-flex">
-                    <button
-                      class="btnArticle d-flex ml-auto px-3 font-weight-bold darkWhite"
-                    >
-                      바로가기
-                    </button>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-  </body>
-</template>
-<style scoped></style>
+<style lang="scss" scoped></style>
