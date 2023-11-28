e6f1e76c3693fe9770ba35b6345fe21292ada76c
31daylee <31daylee@gmail.com>
2023-11-28 12:52:27 +0900

    업데이트 중

---
+++ src/components/molecules/study/ProfileMain.vue
@@ -0,0 +1,31 @@
+<script setup></script>
+<template>
+  <main class>
+    <section>
+      <div class="css-1t4blpu">
+        <aside class="mantine-dhdcaw"><div class="mantine-1v1walg"><div class="mantine-15jussg"><div class="mantine-11lpytq"><div class="mantine-2utty9"><p class="mantine-Text-root mantine-7xhfrq">Bella Lee</p><div class="mantine-Group-root mantine-p9gqvo"><div class="mantine-Group-root mantine-d8k4uu"><p class="mantine-Text-root mantine-idpunc">수강평</p><p class="mantine-Text-root mantine-30beo8">0</p></div><div class="mantine-Group-root mantine-1vo28iy"><p class="mantine-Text-root mantine-idpunc">평균평점</p><p class="mantine-Text-root mantine-30beo8">0.0</p></div></div></div><a aria-label="인프런 프로필 설정 페이지로 이동" href="https://www.inflearn.com/user/settings/account"><div class="mantine-n8y7xk"><div class="mantine-Avatar-root mantine-1o7jgkq"><img class="mantine-9rx0rd mantine-Avatar-image" src="https://cdn.inflearn.com/public/main/profile/default_profile.png" alt="Bella Lee님의 프로필"></div><button class="mantine-UnstyledButton-root mantine-ActionIcon-root mantine-1h3efyk" type="button"><svg aria-hidden="true" focusable="false" data-prefix="far" data-icon="pen-to-square" class="svg-inline--fa fa-pen-to-square " role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" style="font-size: 12px; color: black;"><path fill="currentColor" d="M441 58.9L453.1 71c9.4 9.4 9.4 24.6 0 33.9L424 134.1 377.9 88 407 58.9c9.4-9.4 24.6-9.4 33.9 0zM209.8 256.2L344 121.9 390.1 168 255.8 302.2c-2.9 2.9-6.5 5-10.4 6.1l-58.5 16.7 16.7-58.5c1.1-3.9 3.2-7.5 6.1-10.4zM373.1 25L175.8 222.2c-8.7 8.7-15 19.4-18.3 31.1l-28.6 100c-2.4 8.4-.1 17.4 6.1 23.6s15.2 8.5 23.6 6.1l100-28.6c11.8-3.4 22.5-9.7 31.1-18.3L487 138.9c28.1-28.1 28.1-73.7 0-101.8L474.9 25C446.8-3.1 401.2-3.1 373.1 25zM88 64C39.4 64 0 103.4 0 152V424c0 48.6 39.4 88 88 88H360c48.6 0 88-39.4 88-88V312c0-13.3-10.7-24-24-24s-24 10.7-24 24V424c0 22.1-17.9 40-40 40H88c-22.1 0-40-17.9-40-40V152c0-22.1 17.9-40 40-40H200c13.3 0 24-10.7 24-24s-10.7-24-24-24H88z"></path></svg></button></div></a></div></div><nav><ul class="mantine-1c3lrjn"><li class="mantine-23rrt3"><a aria-label="홈페이지로 이동" href="/users/1252125" style="display: block;"><span class="mantine-Text-root mantine-3m6zv8">•</span><span class="mantine-Text-root mantine-1qwal2x">홈</span></a></li><li class="mantine-3wtmzi"><a aria-label="게시글페이지로 이동" href="/users/1252125/posts" style="display: block;"><span class="mantine-Text-root mantine-mhxzfq">•</span><span class="mantine-Text-root mantine-fozvjm">게시글</span></a></li><li class="mantine-3wtmzi"><a aria-label="블로그페이지로 이동" href="/users/1252125/blogs" style="display: block;"><span class="mantine-Text-root mantine-mhxzfq">•</span><span class="mantine-Text-root mantine-fozvjm">블로그</span></a></li></ul></nav></div></aside>
+      </div>
+    </section>
+  </main>
+</template>
+<style scoped>
+.css-1t4blpu {
+  display: flex;
+  flex: 1 1 0px;
+  padding: 32px;
+  margin: 0px auto;
+  max-width: 1200px;
+}
+.mantine-dhdcaw {
+  border-right: 1px solid #e9ecef;
+  padding-top: 2.5rem;
+  padding-right: 2.5rem;
+}
+.mantine-29kwl2>* {
+  box-sizing: border-box;
+  -webkit-box-flex: 0;
+  -webkit-flex-grow: 0;
+  -ms-flex-positive: 0;
+  flex-grow: 0;
+}
+</style>
