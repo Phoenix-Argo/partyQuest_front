1b25562b34ae2eeb8984a050cfdc6e22b872407e
Jincchus <chopoo2001@gmail.com>
2023-11-30 14:35:58 +0900

    20231130 view

--- src/modules/study/StudyRecruitment.js
+++ src/modules/study/StudyRecruitment.js
@@ -6,13 +6,20 @@ export const useStudyRecruitment = () => {
 
   // 토글 정보
   const handleToggleChange = (selectModifyStudy) => {
-    if (selectModifyStudy.recruitOption == "PNP") {
+
+    if (selectModifyStudy.recruitOption === "PNP") {
+      console.log("toggle1:"+selectModifyStudy.recruitOption)
       return (switchState = ref({ isPnp: true }));
-    }
-    if (selectModifyStudy.recruitOption == "FCFS") {
+      // switchState.value.isPnp = true;
+    }else if (selectModifyStudy.recruitOption === "FCFS") {
+      console.log("toggle2:"+selectModifyStudy.recruitOption)
       return (switchState = ref({ isPnp: false }));
+      // switchState.value.isPnp = false
     }
+    console.log("switchState.isPnp: " + switchState.value.isPnp);
   };
+
+
   // 파티원수
   const member = ref(1);
   const increase = () => {
@@ -40,6 +47,34 @@ export const useStudyRecruitment = () => {
       console.log("count2 : ");
     }
   };
+//
+//   /* modify */
+// // 서버에서 받은 초기 값
+//   const initialRecruitOption = selectModifyStudy.recruitOption;
+//
+//   // 모집 방식 토글(FCFS, PNP)
+//   let switchState = ref({ isPnp: initialRecruitOption === "PNP" });
+//
+//   // 초기화
+//   const initializeToggle = () => {
+//     // 서버에서 받은 값이 PNP이면 true, 그 외에는 false로 초기화
+//     switchState.value.isPnp = initialRecruitOption === "PNP";
+//   };
+//
+//   // 토글 정보
+//   const handleToggleChange = () => {
+//     // 클릭할 때마다 서버에 업데이트
+//     const updatedRecruitOption = switchState.value.isPnp ? "PNP" : "FCFS";
+//
+//     // 여기서 서버에 updatedRecruitOption 값을 업데이트하는 로직을 추가하세요.
+//     // 예를 들어, axios나 fetch 등을 사용하여 서버에 값을 보낼 수 있습니다.
+//
+//     console.log("Updated recruit option:", updatedRecruitOption);
+//   };
+//
+//   // 초기화 함수 호출
+//   initializeToggle();
+
 
   return {
     switchState,
